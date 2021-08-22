802ba9ebd6eaee27271eee38076f25289cdf75c2
Luis Tavares <amanda_fujii@live.com>
2021-08-22 13:06:34 -0300

    bug corretion

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-var character = document.getElementById("character");var block = document.getElementById("block");
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
 function jump(){
-    if(character.classList != "animate"){
-        character.classList.add("animate");
-    }
-  
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
-    },500);
+    },300);
 }
-
-var checkDead = setInterval(function(){
-    var characterTop = 
-    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft = 
-    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>0 % character>=130){
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
         block.style.animation = "none";
-        block.style.display = "none";
-        alert("u lose.");
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
-},10)
+}, 10);
